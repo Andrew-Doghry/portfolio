4c6a201063ec422a2050cf699b51c47120d02469
andrew <andrewshaky50@gamil.com>
2023-02-17 22:37:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let p_btn = document.querySelector('.projects-btn');
+let p_list =document.querySelector('.projects-list');
+p_btn.addEventListener("click",(e)=>{
+        
+        p_list.classList.toggle("hide")
+})
